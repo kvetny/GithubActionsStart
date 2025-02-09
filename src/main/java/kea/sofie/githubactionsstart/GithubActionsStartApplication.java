392a391b2f3883a6fb6513af257c010f9d20ceb4
kvetny <sokv0001@stud.kea.dk>
2025-02-09 04:23:05 +0100

    Initial commit

---
+++ src/main/java/kea/sofie/githubactionsstart/GithubActionsStartApplication.java
@@ -0,0 +1,13 @@
+package kea.sofie.githubactionsstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/sofie/githubactionsstart/GithubActionsStartApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.sofie.githubactionsstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
